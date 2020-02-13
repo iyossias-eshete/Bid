cf2b547a3c0cfa2afe4a20b7422dc8d795130e3f
Iyossias Eshete <iyossias777@gmail.com>
2020-02-13 13:48:00 +0300

    feat/db / feat / migrations / Finished adding db migration files.

---
+++ migrations/20200213111628_bid.js
@@ -0,0 +1,21 @@
+
+exports.up = function (knex) {
+    return knex.schema.createTable('bid', table => {
+        table.increments();
+        table.string('name').notNullable();
+        table.string('description');
+        table.float('startingPrice');
+        table.string('status').notNullable().defaultsTo('Open');
+        table.integer('creatorId')
+            .references('id')
+            .inTable('user')
+            .onDelete('CASCADE')
+            .index();
+
+        table.timestamps(true, true);
+    });
+};
+
+exports.down = function (knex) {
+    return knex.schema.dropTable('bid');
+};
---
+++ migrations/20200213115926_users_bid.js
@@ -0,0 +1,24 @@
+
+exports.up = function (knex) {
+    return knex.schema.createTable('usersBid', table => {
+        table.increments();
+
+        table.integer('userId')
+            .references('id')
+            .inTable('user')
+            .onDelete('CASCADE')
+            .index();
+
+        table.integer('bidId')
+            .references('id')
+            .inTable('bid')
+            .onDelete('CASCADE')
+            .index();
+
+        table.timestamps(true, true);
+    });
+};
+
+exports.down = function (knex) {
+    return knex.schema.dropTable('usersBid');
+};
